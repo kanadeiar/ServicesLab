2167516b58242714b6a1da5b84ae620a00bbf6fb
kanadeiar <kanadeiar@gmail.com>
2023-09-01 07:36:30 +0300

    Инит приложения

---
+++ ServicesLab/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лаборатория по разработке сервисных приложений");
+
+
+            Console.WriteLine("Нажать любую кнопку для завершения работы ...");
+            Console.ReadKey();
+        }
+    }
+}
