6a665f2bc5017961f8f3c13ce2332b37f02d96c7
kanadeiar <kanadeiar@gmail.com>
2023-09-11 00:29:44 +0300

    TCP

--- ServicesLab/ConsoleApp1/Program.cs
+++ ServicesLab/ConsoleApp1/Program.cs
@@ -1,13 +1,81 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Handler
+    {
+        private readonly TcpClient _client;
+
+        public Handler(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task RunClient()
+        {
+            var reader = new StreamReader(_client.GetStream());
+            var writer = new StreamWriter(_client.GetStream());
+
+            var returnData = await reader.ReadLineAsync();
+            var userName = returnData;
+            Console.WriteLine($"User {userName} connect to server");
+            
+            while (true)
+            {
+                returnData = await reader.ReadLineAsync();
+                if (returnData.IndexOf("QUIT", StringComparison.Ordinal) > -1)
+                {
+                    Console.WriteLine($"User {userName} is disconnected from server");
+                    break;
+                }
+
+                Console.WriteLine($"{userName} : {returnData}");
+
+                var bytes = Encoding.UTF8.GetBytes(returnData);
+                await writer.WriteAsync(Encoding.UTF8.GetChars(bytes));
+
+            }
+
+            _client.Close();
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        const int Port = 8080;
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Лаборатория по разработке сервисных приложений");
+            var listener = new TcpListener(IPAddress.Any, Port);
+            try
+            {
+                listener.Start();
+                Console.WriteLine("Ожидание подключений ...");
 
+                while (true)
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+
+                    var handler = new Handler(client);
+
+                    Task.Run(() => handler.RunClient());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Stop();
+            }
 
 
             Console.WriteLine("Нажать любую кнопку для завершения работы ...");
--- ServicesLab/ConsoleAppClient/Program.cs
+++ ServicesLab/ConsoleAppClient/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace ConsoleAppClient;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleAppClient;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Клиентское приложение");
+
+        var point = new IPEndPoint(IPAddress.Loopback, 8101);
+        var client = new TcpClient(point);
+        await client.ConnectAsync(IPAddress.Loopback, 8100);
+
+        var stream = client.GetStream();
+        var byt1 = Encoding.UTF8.GetBytes("Сообщение на сервер<END>");
+        await stream.WriteAsync(byt1);
+
+        await Task.Delay(500);
         
+        var bytesRead = new byte[client.ReceiveBufferSize];
+        await stream.ReadAsync(bytesRead, 0, client.ReceiveBufferSize);
+        await stream.FlushAsync();
+        Console.WriteLine($"Получено с сервера: {Encoding.UTF8.GetString(bytesRead)}");
+
+        client.Close();
+
         Console.WriteLine("Нажать любую кнопку для завершения работы ...");
         Console.ReadKey();
     }
